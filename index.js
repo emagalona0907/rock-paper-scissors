4c3eefe256dcfa15d3305f8bc2283e725c4563c0
Emmanuel Magalona <183299410+emagalona0907@users.noreply.github.com>
2024-10-29 04:01:09 +0800

    Added visibility of Selections
    Added a table that shows each selection of player per round.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function getComputerSelection(){
-    return Math.random() * (3 - 1) + 1;
+    return Math.round(Math.random() * (3 - 1) + 1);
 }
 
 function getPlayerSelection(){
     let playerInput = prompt("Select from rock, paper, scissors. Please input 1 for Rock, 2 for Paper, 3 for scissors.");
 
-    return playerInput;
+    return parseInt(playerInput);
 }
 
 function substituteSelectionToText(playerInput){
@@ -13,17 +13,17 @@ function substituteSelectionToText(playerInput){
     let selection;
    
     switch(playerInput){
-        case "1":
+        case 1:
             selection = "Rock";
             break;
-        case "2":
+        case 2:
             selection = "Paper";
             break;
-        case "3":
+        case 3:
             selection = "Scissors";
             break;
         default:
-            selection = "not available"
+            selection = "not available";
     }
     
     return selection;
@@ -85,21 +85,58 @@ function playRound(){
     return roundWinner;
 }
 
+function displayResults(score1, score2){
+
+    console.log("Game Status");
+    console.log(`Player Score: ${score1}`);
+    console.log(`Computer Score Score: ${score2}`);
+
+    
+    alert(`Game Status ---Player Score: ${score1} ---Computer Score Score: ${score2}`);
+   
+}
+
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
 
-    while (playerScore !== 5 || computerScore !== 5){
-        let winner = playRound();
+    while (playerScore != 5 || computerScore != 5){
+        
+        
+        if (playerScore == 5 || computerScore == 5){
+            if (playerScore == 5) {
+                alert("Player Won this game!");
+            } else {
+                alert("Computer Won the game!");
+            }
+            
+            break;
+        } else {
+            let winner = playRound();
 
-        switch(winner){
-            case "player":
-                incrementScore(playerScore);
-                break;
-            case "computer":
-                incrementScore(computerScore);
-                break;
+            switch(winner){
+                case "player":
+                    playerScore++;
+                    alert("Player Wins!");
+                    displayResults(playerScore, computerScore);
+                    break;
+                case "computer":
+                    computerScore++;
+                    alert("Computer Wins!")
+                    displayResults(playerScore, computerScore);
+                    break;
+                case "tie":
+                    playerScore++;
+                    computerScore++;
+                    alert("It's a tie!");
+                    displayResults(playerScore, computerScore);
+                    break;
+            }
         }
-
     }
+
+    
 }
+
+
+playGame();
